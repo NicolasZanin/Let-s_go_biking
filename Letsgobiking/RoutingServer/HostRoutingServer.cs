23d18b6982361f7b81c316f4c1220b404c6d96f6
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2023-11-20 12:52:44 +0100

    OSM

--- Letsgobiking/RoutingServer/HostRoutingServer.cs
+++ Letsgobiking/RoutingServer/HostRoutingServer.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.ServiceModel.Description;
 using System.ServiceModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RoutingServer {
     class HostRoutingServer  {
-        static void Main(string[] args) { 
+        static OpenStreetMapManager OM = new OpenStreetMapManager();
+
+        public static async Task<JsonDocument> ComputeItineraire(string start, string end, string locomotion)
+        {
+            return await OM.ComputeItineraire(start,end,locomotion);
+        }
+        static async Task Main(string[] args) {
+
             Uri httpUrl = new Uri("http://localhost:8091/IServiceRoutingServer/ServiceRoutingServer");
             
             //Create ServiceHost
@@ -32,6 +42,7 @@ namespace RoutingServer {
             }
             Console.WriteLine("Service is host at " + DateTime.Now.ToString());
             Console.WriteLine("Host is running... Press <Enter> key to stop");
+            Console.WriteLine(ComputeItineraire("7.5043,43.7765", "7.2661, 43.7031", "cycling-regular"));
             Console.ReadLine();
             host.Close();
         }
---
+++ Letsgobiking/RoutingServer/OpenStreetMapManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RoutingServer
+{
+    class OpenStreetMapManager
+    {
+        public async Task<JsonDocument> ComputeItineraire(string start, string end, string locomotion)
+        {
+            HttpClient OSMapi = new HttpClient();
+            string apiUrl = "https://api.openrouteservice.org/v2/directions/" + locomotion + "?api_key=5b3ce3597851110001cf6248ef43ce34eac546a6b118b6706bfe11ce&start=" + start + "&end=" + end;
+            HttpResponseMessage response = await OSMapi.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JsonDocument document = JsonDocument.Parse(responseBody);
+            return document;
+        }
+    }
+}
